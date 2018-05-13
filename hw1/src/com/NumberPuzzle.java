6d9f1c910a4095694943e677f81af9cf33047908
Anas Abou Allaban <piraka9011@hotmail.com>
2018-05-12 21:43:19 -0400

    idk what to do next

--- hw1/src/com/NumberPuzzle.java
+++ hw1/src/com/NumberPuzzle.java
@@ -18,6 +18,7 @@ public class NumberPuzzle {
     private int blank_r, blank_c;   // blank row and column
 
     private int[][] solution;
+    private Node[][] node_tiles;
 
     public static void main(String[] args) {
         NumberPuzzle myPuzzle = readPuzzle();
@@ -87,6 +88,32 @@ public class NumberPuzzle {
 
 /*------------------ Implementation ------------------*/
 
+    class Edge {
+        private int cost = 0;
+        private Node target;
+
+        Edge (Node targetNode) {
+            target = targetNode;
+        }
+    }
+
+    class Node {
+        private int value;
+        private int x;
+        private int y;
+        private int h;
+        private int f = 0;
+        private int g;
+        private Edge[] neighbors;
+        private Node parent;
+
+        Node(int val, int row, int col) {
+            value = val;
+            x = row;
+            y = col;
+        }
+    }
+
     public int[] getGoalCoord(int num) {
         // Check bounds
         if (num < 0 || num > 15)
@@ -106,6 +133,37 @@ public class NumberPuzzle {
         return coords;
     }
 
+    public void initNodes() {
+        for (int i = 0; i < PUZZLE_WIDTH; i++) {
+            for (int j = 0; j < PUZZLE_WIDTH; j++) {
+                node_tiles[i][j] = new Node(tiles[i][j], i, j);
+            }
+        }
+    }
+
+    public void initNeighbors() {
+        for (int i = 0; i < PUZZLE_WIDTH; i++) {
+            for (int j = 0; j < PUZZLE_WIDTH; j++) {
+                node_tiles[i][j].neighbors = new Edge[]{
+                        new Edge(node_tiles[i+1][j]),   // Top
+                        new Edge(node_tiles[i-1][j]),   // Bottom
+                        new Edge(node_tiles[i][j+1]),   // right
+                        new Edge(node_tiles[i][j-1]),   // Left
+                        new Edge(node_tiles[i+1][j+1]), // Diagonals
+                        new Edge(node_tiles[i+1][j-1]),
+                        new Edge(node_tiles[i-1][j+1]),
+                        new Edge(node_tiles[i-1][j-1])
+                }
+            }
+        }
+    }
+//    public int getManhattanDistance(Node n) {
+//        int[] goalPos = getGoalCoord(n.value);
+//        int goalRow = goalPos[0], goalCol = goalPos[1];
+//        int result = Math.abs(goalRow - n.position[0]) + Math.abs(goalCol - n.position[1]);
+//        return result;
+//    }
+
     public int manhattanHeuristic(int currRow, int currCol) {
         // Get the number we're at now
         int currNum = tiles[currRow][currCol];
@@ -116,6 +174,35 @@ public class NumberPuzzle {
         int result = Math.abs(goalRow - currRow) + Math.abs(goalCol - currCol);
         return result;
     }
+
+    private PriorityQueue<Node> initQueue() {
+        return new PriorityQueue<>(32, new Comparator<Node>() {
+            public int compare(Node n1, Node n2) {
+                if (n1.f > n2.f)
+                    return 1;
+                else if (n1.f < n2.f)
+                    return -1;
+                else
+                    return 0;
+            }
+        });
+    }
+
+//    private PriorityQueue<Node> initManhattanQueue() {
+//        return new PriorityQueue<>(32, new Comparator<Node>() {
+//            public int compare(Node o1, Node o2) {
+//                int d1 = getManhattanDistance(o1);
+//                int d2 = getManhattanDistance(o2);
+//                if (d1 < d2)
+//                {
+//                    return 1;
+//                }
+//                return 0;
+//            }
+//        });
+//    }
+
+
 
     // Heuristic that returns the number of tiles out of place.
     public int placeHeuristic() {
@@ -135,7 +222,36 @@ public class NumberPuzzle {
     // betterH:  if false, use tiles-out-of-place heuristic
     //           if true, use total-manhattan-distance heuristic
     LinkedList<NumberPuzzle> solve(boolean betterH) {
-        PriorityQueue<Integer> queue = new PriorityQueue<Integer>();
+        // Setup
+        Node current;
+        int[] goalCoordinates;
+        initNodes();
+        initNeighbors();
+
+        // Init Queue and explored nodes
+        Set<Node> exploredNodes = new HashSet<Node>();
+        Queue<Node> priorityQueue = initQueue();
+
+        // Set starting node
+        Node startNode = new Node(BLANK, blank_r, blank_c);
+        priorityQueue.add(startNode);
+
+        // Aystaaah
+        while(!priorityQueue.isEmpty()) {
+            current = priorityQueue.poll();    // Remove lowest cost node
+
+            // If goal is reached, return
+            if (solved()) {
+                break;
+            }
+
+            // For each neighbor of n:
+            for (Edge neighbor : current.neighbors) {
+                Node child = neighbor.target;
+                int cost = neighbor.cost;
+                
+            }
+        }
         return new LinkedList<NumberPuzzle>();
     }
 
